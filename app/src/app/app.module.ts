bda7056645b84bba1deb3876aa304ed1786c4212
Thiago Ribeiro <thiagobcoelho25@gmail.com>
2023-01-22 11:55:03 -0300

    criação estilo tela home

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuLeftBarComponent } from './components/menu-left-bar/menu-left-bar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuLeftBarComponent,
+    HomeComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
---
+++ app/src/app/components/menu-left-bar/menu-left-bar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-left-bar',
+  templateUrl: './menu-left-bar.component.html',
+  styleUrls: ['./menu-left-bar.component.css']
+})
+export class MenuLeftBarComponent {
+
+}
