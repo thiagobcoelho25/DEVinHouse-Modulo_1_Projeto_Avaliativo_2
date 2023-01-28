56a22fed731fdd52a79c47225e40294bf0662b0b
Thiago Ribeiro <thiagobcoelho25@gmail.com>
2023-01-28 17:05:46 -0300

    tela prontuario: implementação finalizada

--- app/src/app/pages/medical-record/medical-record.component.ts
+++ app/src/app/pages/medical-record/medical-record.component.ts
@@ -1,8 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-import { concat, delay, finalize, switchMap } from 'rxjs';
+import { delay, switchMap } from 'rxjs';
 import { BackendService } from 'src/app/services/backend.service';
+import { Consulta } from 'src/app/shared/interfaces/consulta';
+import { Exame } from 'src/app/shared/interfaces/exame';
 import { PacienteProntuario } from 'src/app/shared/interfaces/paciente_prontuario';
+
+interface ExameConsulta {
+  id: number,
+  pacienteId: number
+  data: string,
+  hora: string
+  motivo: string | null,
+  descricao_do_problema: string | null,
+  medicacao_receitada: string | null,
+  dosagem_e_precaucoes: string | null,
+  nome_do_exame: string | null,
+  tipo_do_exame: string | null,
+  laboratorio: string | null,
+  url_do_documento_do_exame: string | null,
+  resultado_do_exame: string | null,
+  tipo: string
+}
 
 @Component({
   selector: 'app-medical-record',
@@ -31,10 +50,52 @@ export class MedicalRecordComponent implements OnInit {
         this.erro_dados = true
         console.log('Error da Inicialização do prontuario:', err)
       }
-    }).add( () => {
+    }).add(() => {
       this.carregando_dados = false
     })
-    
+
+  }
+
+  get exames_consultas_prontuario() {
+    const exames_consultas: ExameConsulta[] = [...this.paciente_prontuario.consultas, ...this.paciente_prontuario.exames].map(ele => {
+      if (ele.hasOwnProperty('data_da_consulta')) {
+        const ele_consulta = ele as Consulta
+        return {
+          data: ele_consulta.data_da_consulta,
+          hora: ele_consulta.hora_da_consulta,
+          id: ele_consulta.id,
+          motivo: ele_consulta.motivo,
+          descricao_do_problema: ele_consulta.descricao_do_problema,
+          medicacao_receitada: ele_consulta.medicacao_receitada,
+          dosagem_e_precaucoes: ele_consulta.dosagem_e_precauções,
+          pacienteId: ele_consulta.pacienteId,
+          tipo: "consulta"
+        } as ExameConsulta
+      } else { 
+        const ele_exame = ele as Exame
+        return {
+          id: ele_exame.id,
+          data: ele_exame.data_do_exame,
+          hora: ele_exame.hora_do_exame,
+          laboratorio: ele_exame.laboratorio,
+          nome_do_exame: ele_exame.nome_do_exame,
+          resultado_do_exame: ele_exame.resultado_do_exame,
+          tipo_do_exame: ele_exame.tipo_do_exame,
+          url_do_documento_do_exame: ele_exame.url_do_documento_do_exame,
+          pacienteId: ele_exame.pacienteId,
+          tipo: "exame"
+        } as ExameConsulta
+      }
+    })
+
+    return exames_consultas.sort((a, b) => {
+        return Date.parse(a.data) - Date.parse(b.data)
+      })
   }
 
 }
+
+
+// descricao_do_problema: string,
+//     medicacao_receitada: string,
+//     dosagem_e_precauções
--- app/src/app/shared/interfaces/paciente_prontuario.ts
+++ app/src/app/shared/interfaces/paciente_prontuario.ts
@@ -1,3 +1,5 @@
+import { Consulta } from "./consulta";
+import { Exame } from "./exame";
 import { Pacientes } from "./paciente";
 
 export interface PacienteProntuario extends Pacientes {
@@ -5,25 +7,3 @@ export interface PacienteProntuario extends Pacientes {
     exames: Exame[]
 }
 
-interface Consulta {
-    id: number,
-    motivo: string,
-    data_da_consulta: string,
-    hora_da_consulta: string,
-    descricao_do_problema: string,
-    medicacao_receitada: string,
-    dosagem_e_precauções: string,
-    pacienteId: number
-}
-
-interface Exame {
-    id: number,
-    nome_do_exame: string,
-    data_do_exame: string,
-    hora_do_exame: string,
-    tipo_do_exame: string,
-    laboratorio: string,
-    url_do_documento_do_exame: string,
-    resultado_do_exame: string,
-    pacienteId: number
-}
